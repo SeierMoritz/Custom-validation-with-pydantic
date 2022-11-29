1a9a8db7613b8f91ab6a969027af28134c2fae6c
SeierMoritz <moritz.seier@gmail.com>
2022-11-29 17:01:49 +0100

    Stress 2.0

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, ValidationError, validator
 
 app = FastAPI()
 
 @app.get('/')
 def home():
     return {"message": "Werden Sie gesund."}
+
